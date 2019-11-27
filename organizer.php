c31a1ff999143f0141ddf7cee93ec114b7994601
daguila93 <edildaguila@outlook.com>
2019-11-27 00:19:28 -0300

    WIP: Added already exists directory test

--- organizer.php
+++ organizer.php
@@ -1,9 +1,11 @@
 <?php
+
 	function getPath(){
 		$dirPathRoot;
 		do{
 			$dirPathRoot = readline("Type a valid directory's path: ");
-			//if the string is equals to '' the realpath function gives the current path.
+			//if the string is equals to '' the realpath function gives the current path. 
+			//So I needed to change it to a false value like: 'Wrong path'
 			if ($dirPathRoot === '') {
 				$dirPathRoot = 'Wrong path';
 			}
@@ -41,7 +43,14 @@
 
 	function createDirectory($path, $extensionsArray){
 		foreach ($extensionsArray as $value) {
-			mkdir($path.DIRECTORY_SEPARATOR.$value);
+			//if the directory was already created, the software will not create again
+			if (!is_dir($path.DIRECTORY_SEPARATOR.$value.DIRECTORY_SEPARATOR)) {
+				//create the directory
+				mkdir($path.DIRECTORY_SEPARATOR.$value);
+				echo "The directory $value was successfully created. \n";
+			} else {
+				echo "The directory $value was already created. \n";
+			}
 		}
 	}
 
