7f40c30d0769e21baa33dea5f58d0f662b170046
daguila93 <edildaguila@outlook.com>
2019-12-27 19:21:00 -0300

    WIP by daguila93: Evolving the function fileMove.

--- organizer.php
+++ organizer.php
@@ -1,8 +1,7 @@
 <?php
 
 	function getPath(){
-		$dirPathRoot;
-		do{
+        do{
 			$dirPathRoot = readline("Type a valid directory's path: ");
 			//if the string is equals to '' the realpath function gives the current path. 
 			//So I needed to change it to a false value like: 'Wrong path'
@@ -15,10 +14,10 @@
 		return $dirPathRoot;
 	}
 
-	function getFileExtension($path){
+	function fileHandler($path, $callback){
 		//Create an array with all files and directories
 		$array = scandir($path);
-		$fileExtensions = array();
+		$genericReturn = array();
 
 		foreach ($array as $value) {
 
@@ -29,19 +28,30 @@
 				if (is_file($path.DIRECTORY_SEPARATOR.$value)) {
 					//pathinfo get a lot of information about the file, you need to specify 
 					$path_parts = pathinfo($value);
-					//add extension to the array if it is not there.
-					if(!in_array($path_parts['extension'], $fileExtensions)){
-						array_push($fileExtensions, $path_parts['extension']);
-					}
-				}
-				
+                    $genericReturn = $callback($path_parts, $genericReturn);
+                }
+
 			}
-			
+
 		}
-		return $fileExtensions;
+		return $genericReturn;
 	}
 
-	function createDirectory($path, $extensionsArray){
+/**
+ * @param array $path_parts
+ * @param array $fileExtensions
+ * @return array
+ */
+function getFileExtensions(array $path_parts, array $fileExtensions)
+{
+//add extension to the array if it is not there.
+    if (!in_array($path_parts['extension'], $fileExtensions)) {
+        array_push($fileExtensions, $path_parts['extension']);
+    }
+    return $fileExtensions;
+}
+
+function createDirectory($path, $extensionsArray){
 		foreach ($extensionsArray as $value) {
 			//if the directory was already created, the software will not create again
 			if (!is_dir($path.DIRECTORY_SEPARATOR.$value.DIRECTORY_SEPARATOR)) {
@@ -54,10 +64,41 @@
 		}
 	}
 
+	function moveFiles($path){
+
+        //File chose to be moved;
+        $fileToMove = realpath( DIRECTORY_SEPARATOR . 'Users' . DIRECTORY_SEPARATOR. 'edild' . DIRECTORY_SEPARATOR . 'Desktop' . DIRECTORY_SEPARATOR . 'teste' . DIRECTORY_SEPARATOR . 'R20191213.pdf');
+        //Place where the File will be moved.
+        $placeToMove = realpath( DIRECTORY_SEPARATOR . 'Users' . DIRECTORY_SEPARATOR. 'edild' . DIRECTORY_SEPARATOR . 'Desktop' . DIRECTORY_SEPARATOR . 'teste' . DIRECTORY_SEPARATOR . 'pdf' . DIRECTORY_SEPARATOR);
+
+        //Unix command to move something
+        $cmd = 'mv ' . $fileToMove . ' ' . $placeToMove;
+
+        //exec() is a command that execute something
+        exec($cmd, $output, $return_val);
+
+        //test if returned success or not.
+        if ($return_val == 0) {
+            echo "success! \n";
+        } else {
+            echo "failed, aborting... \n";
+        }
+
+
+//         $newplace = DIRECTORY_SEPARATOR . $path['extension'] . DIRECTORY_SEPARATOR . $path['basename'];
+//             if(rename($path['dirname'], $newplace)){
+//                 return $newplace;
+//             }
+//         return null;
+    }
+
 	function organizer(){
 		$path = getPath();
-		$fileExtensions = getFileExtension($path);
-		createDirectory($path, $fileExtensions);
+        $fileExtensions = fileHandler($path, 'getFileExtensions');
+        createDirectory($path, $fileExtensions);
+        //move files to the respective places
+        fileHandler($path, 'moveFiles');
 	}
 
 	//C:\Users\edild\Downloads
+    //C:\Users\edild\Desktop\teste
